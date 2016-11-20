4630961fa19f61aa1219b9d36049826928d84a3a
Gabriel Sánchez <gslu2717@gmail.com>
2016-11-20 10:04:15 -0500

    Gestos touch y font awesome

--- js/main.js
+++ js/main.js
@@ -22,3 +22,18 @@ function toggleMenu(){
     $menu.classList.toggle('active');
     // document.getElementById('burguer-button').innerHTML = '&#xf00d;';
 }
+function showMenu(){
+    $menu.classList.add('active');
+    // document.getElementById('burguer-button').innerHTML = '&#xf00d;';
+}
+function hideMenu(){
+    $menu.classList.remove('active');
+    // document.getElementById('burguer-button').innerHTML = '&#xf00d;';
+}
+
+//Gestos touch
+var $body = document.body;
+
+var gestosTouch = new Hammer($body);
+gestosTouch.on('swipeleft', showMenu);
+gestosTouch.on('swiperight', hideMenu);
